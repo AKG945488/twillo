1985afaa64bd0e9630956673bac89f857a8c7567
AKG945488 <105988316+AKG945488@users.noreply.github.com>
2022-07-16 05:53:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php 
-require_once('vendor/autoload.php');
-$stripe = new \Stripe\StripeClient('sk_test_nR7fNrOKz8OAVLg8nIlLNEeu00CxbYwI5v');
-
-$refunded = $stripe->refunds->create([
-    'charge' => 'ch_1Hedw2EecuKiqt1LZc2qCra7',
-    'amount' => 3000
-  ]);
-
-echo '<pre>';
-  print_r($refunded);
-echo '</pre>';
-
-
-
-
-
-
-
-?>
+ 
+// Update the path below to your autoload.php, 
+// see https://getcomposer.org/doc/01-basic-usage.md 
+require_once '/vendor/autoload.php'; 
+ 
+use Twilio\Rest\Client; 
+ 
+$sid    = "ACf3ff8f9f8d9f41ebe70c9a0a37c30dc0"; 
+$token  = "[AuthToken]"; 
+$twilio = new Client($sid, $token); 
+ 
+$message = $twilio->messages 
+                  ->create("whatsapp:+918894002117", // to 
+                           array( 
+                               "from" => "whatsapp:+14155238886",       
+                               "body" => "maderchod" 
+                           ) 
+                  ); 
+ 
+print($message->sid);
